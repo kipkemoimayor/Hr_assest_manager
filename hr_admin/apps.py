b37b9b015a47ee5fba42129d3f94983774bcfbdb
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-08-01 22:22:27 +0300

    Innitial commit

---
+++ hr_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HrAdminConfig(AppConfig):
+    name = 'hr_admin'
---
+++ hr_admin/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    title="Admin"
+
+    return render(request,"index.html",{"title":title})

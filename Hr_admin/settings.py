def5181cb0924ace98edc6475e2c413bf44f3e0e
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-08-09 14:35:27 +0300

    Masked pass

--- Hr_admin/settings.py
+++ Hr_admin/settings.py
@@ -33,9 +33,9 @@ Email Registratiom
 '''
 
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='angeldilorna@gmail.com'
+EMAIL_HOST_USER=''
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_PASSWORD='Collins@24'
+EMAIL_HOST_PASSWORD='*******'
 EMAIL_PORT=587
 
 AUTH_USER_MODEL = 'hr_admin.CustomUser'
@@ -92,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hr',
-        'PASSWORD':'collo',
-        'USER':'collo',
+        'PASSWORD':'*****',
+        'USER':'****',
 
     }
 }
